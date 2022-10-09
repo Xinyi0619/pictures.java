43418d5ba14982b2e980a7906ecf0c30a93fd7df
yxy <1146809172@qq.com>
2022-10-09 20:28:00 +0800

    Initial commit

---
+++ src/XIN/xinxin.java
@@ -0,0 +1,12 @@
+package XIN;
+
+public class xinxin {
+    public static void main(String[] args) {
+       int x=1;
+       for(int i=0;i<1;i++) {
+       }
+
+        System.out.println(x);
+
+    }
+}
